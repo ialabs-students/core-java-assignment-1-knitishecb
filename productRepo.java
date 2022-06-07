b995b66681b338bb499c77f8a4e14cd3cf888b45
knitishecb <59618543+knitishecb@users.noreply.github.com>
2022-06-07 17:07:21 +0530

    Create productRepo.java
    commited

---
+++ productRepo.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ProductService {
+
+	ProductRepository productRepository = new ProductRepository();
+	List<Product> plist = productRepository.getList();
+	public ProductService() {
+		
+	}
+	String findNameByCode(int productCode) { 
+		for(Product product : plist) {
+			if(product.getProductCode()==productCode) {
+				return product.getName();
+			}
+		}
+		 return null;
+	}
+	
+	
+	Product findMaxPriceProduct(String category) {
+		List<Product> pro1 = new ArrayList<Product>();
+		for(Product product: plist) {
+			if(product.getCategory()==category) {
+				pro1.add(product);
+			}
+		}
+		if(pro1.isEmpty()) {
+			return null;
+		}
+		Product productMax = pro1.stream().max(Comparator.comparingDouble(Product::getPrice)).get();
+		return productMax;
+	}
+	
+	
+	ArrayList<Product> getProductsByCategory(String category) {
+		ArrayList<Product> pro1 = new ArrayList<Product>();
+		for(Product product: plist) {
+			if(product.getCategory()==category) {
+				pro1.add(product);
+			}
+		}
+		if(pro1.isEmpty()) {
+			return null;
+		}
+		return pro1;
+	}
+	public static void main(String[] args) {
+		ProductService product = new ProductService();
+		System.out.println(product.findNameByCode(102));
+		System.out.println(product.findMaxPriceProduct("electronic"));
+		System.out.println(product.findMaxPriceProduct("clothes"));
+		System.out.println(product.getProductsByCategory("electronic"));
+	}
+	
+}
